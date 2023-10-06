d0985b73742c310554e80e166fd2fe3430a0c606
NIXX <git@nixx.dev>
2023-10-05 22:24:42 -0400

    feat: Add Headless UI and Heroicons to project
    - Integrate Headless UI library to enhance UI components
    - Include Heroicons for ready-to-use SVG icons

--- frontend/src/components/layout/AuthLayout.tsx
+++ frontend/src/components/layout/AuthLayout.tsx
@@ -1,20 +1,20 @@
-import { Outlet, Navigate } from "react-router-dom";
-import Navbar from "../ui/Navbar";
-import useAuthContext from "../../hooks/useAuthContext";
+import { Outlet, Navigate } from 'react-router-dom';
+import Navbar from '../ui/Navbar';
+import useAuthContext from '../../hooks/useAuthContext';
 
 export default function AuthLayout() {
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
 
-  return user ?
-    (
-      <div className="flex flex-col h-screen">
-        <Navbar />
-        <div className="h-full py-10">
-          <div className="container mx-auto px-4">
-            < Outlet />
-          </div>
+  return user ? (
+    <div className="flex flex-col h-screen bg-slate-100">
+      <Navbar />
+      <div className="h-full py-10">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <Outlet />
         </div>
       </div>
-    ) :
+    </div>
+  ) : (
     <Navigate to={'/login'} />
+  );
 }
